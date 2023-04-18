318a785bcbb203e632fa6663cbc98cc4597c0672
zosmac <zosmac@github.com>
2023-04-17 20:16:17 -0500

    distinguish tree node and comparator types

--- tree.go
+++ tree.go
@@ -10,14 +10,17 @@ type (
 	// Node is the comparable type for each node of Tree.
 	Node interface{ ~int | ~string }
 
+	// Comparator is a value returned by the Order function of a Table for sorting.
+	Comparator interface{ ~int | ~string }
+
 	// Table refers to the data corresponding the nodes of a tree.
 	Table[N Node, V any] map[N]V
 
 	// Tree defines a hierarchy of Nodes of comparable type.
-	Tree[N, C Node, V any] map[N]Tree[N, C, V]
+	Tree[N Node, C Comparator, V any] map[N]Tree[N, C, V]
 
 	// Order distinguishes similar Node values for sorting.
-	Order[N, C Node, V any] func(N, Table[N, V]) C
+	Order[N Node, C Comparator, V any] func(N, Table[N, V]) C
 
 	// Display shows the value of a Node at particular depth in Tree.
 	Display[N Node, V any] func(int, N, Table[N, V])
@@ -33,34 +36,19 @@ func (tr Tree[N, C, V]) Add(nodes ...N) {
 	}
 }
 
-// Sortnodes sorts the top nodes of a tree and returns as an ordered slice.
-func (tr Tree[N, C, V]) Sortnodes(tbl Table[N, V], fn Order[N, C, V]) []N {
-	var nodes []N
-	for node := range tr {
-		nodes = append(nodes, node)
-	}
-	sort.Slice(nodes, func(i, j int) bool {
-		nodei := fn(nodes[i], tbl)
-		nodej := fn(nodes[j], tbl)
-		return nodei < nodej ||
-			nodei == nodej && nodes[i] < nodes[j]
-	})
-	return nodes
-}
-
 // Traverse walks the tree and invokes function fn for each node.
-func (tr Tree[N, C, V]) Traverse(depth int, tbl Table[N, V], fn1 Order[N, C, V], fn2 Display[N, V]) {
-	for _, node := range tr.Sortnodes(tbl, fn1) {
-		fn2(depth, node, tbl)
-		tr[node].Traverse(depth+1, tbl, fn1, fn2)
+func (tr Tree[N, C, V]) Traverse(depth int, tbl Table[N, V], order Order[N, C, V], display Display[N, V]) {
+	for _, node := range tr.order(tbl, order) {
+		display(depth, node, tbl)
+		tr[node].Traverse(depth+1, tbl, order, display)
 	}
 }
 
 // FlatTree walks the tree to build an ordered slice of the nodes.
-func (tr Tree[N, C, V]) FlatTree(tbl Table[N, V], fn Order[N, C, V]) []N {
+func (tr Tree[N, C, V]) Flatten(tbl Table[N, V], order Order[N, C, V]) []N {
 	var flat []N
-	for _, node := range tr.Sortnodes(tbl, fn) {
-		flat = append(append(flat, node), tr[node].FlatTree(tbl, fn)...)
+	for _, node := range tr.order(tbl, order) {
+		flat = append(append(flat, node), tr[node].Flatten(tbl, order)...)
 	}
 	return flat
 }
@@ -76,4 +64,19 @@ func (tr Tree[N, C, V]) FindTree(node N) Tree[N, C, V] {
 		}
 	}
 	return nil
+}
+
+// order sorts the top nodes of a tree and returns as an ordered slice.
+func (tr Tree[N, C, V]) order(tbl Table[N, V], fn Order[N, C, V]) []N {
+	var nodes []N
+	for node := range tr {
+		nodes = append(nodes, node)
+	}
+	sort.Slice(nodes, func(i, j int) bool {
+		nodei := fn(nodes[i], tbl)
+		nodej := fn(nodes[j], tbl)
+		return nodei < nodej ||
+			nodei == nodej && nodes[i] < nodes[j]
+	})
+	return nodes
 }
